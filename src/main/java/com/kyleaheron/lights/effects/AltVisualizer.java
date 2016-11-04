fe6c57c78b745fcd57c4f92fdd692f31e28e16f1
Kyle Aheron <kyle98750@gmail.com>
2016-11-03 21:33:44 -0400

    Redesigned the new visualizer setting

--- src/main/java/com/kyleaheron/lights/effects/AltVisualizer.java
+++ src/main/java/com/kyleaheron/lights/effects/AltVisualizer.java
@@ -28,6 +28,7 @@ public class AltVisualizer implements IEffect{
     private static FFT fft = new FFT();
     private static PowerSpectrum powerSpectrum = new PowerSpectrum();
     private static SpectralCentroid centroid = new SpectralCentroid(audioContext.getSampleRate());
+    private static Frequency frequency = new Frequency(audioContext.getSampleRate());
 
     public AltVisualizer() {
         masterGain.addInput(stereoInput);
@@ -35,78 +36,115 @@ public class AltVisualizer implements IEffect{
         segmenter.addListener(fft);
         fft.addListener(powerSpectrum);
         powerSpectrum.addListener(centroid);
+        powerSpectrum.addListener(frequency);
         audioContext.out.addDependent(segmenter);
     }
 
     private float fMinAvg = 0, fMaxAvg = 0, fMinSum = 0, fMaxSum = 0;
     private float pMinAvg = 0, pMaxAvg = 0, pMinSum = 0, pMaxSum = 0;
-    private int numOfSamples;
+    private float cfMinAvg = 0, cfMaxAvg = 0, cfMinSum = 0, cfMaxSum = 0;
+    private int numOfSamples = 1;
 
     @Override
     public void show() {
-        if (!audioContext.isRunning()) audioContext.start();
-        float[][] features = fft.getFeatures();
-        if (features != null) {
-            for (float[] sample : features) {
-                int bin = 0;
-                float pMin = 0;
-                float pMax = 0;
-                float fMin = 0;
-                float fMax = 0;
-                if (sample != null && sample == features[0]) {
-                    for (float amplitude : sample) {
-                        float frequency = FFT.binFrequency(audioContext.getBufferSize(), 1024, bin);
-                        if (amplitude > pMax) {
-                            pMax = amplitude;
-                            fMax = frequency;
-                        } else if (amplitude < pMin && amplitude > 0.5) {
-                            pMin = amplitude;
-                            fMin = frequency;
-                        }
-                        bin++;
-                    }
-
-                    fMaxSum += fMax;
-                    fMinSum += fMin;
-                    fMaxAvg = fMaxSum / numOfSamples;
-                    fMinAvg = fMinSum / numOfSamples;
-
-                    pMaxSum += pMax;
-                    pMinSum += pMin;
-                    pMaxAvg = pMaxSum / numOfSamples;
-                    pMinAvg = pMinSum / numOfSamples;
-
-                    if (numOfSamples < 300) {
-                        numOfSamples++;
-                    } else {
-                        numOfSamples = 1;
-                    }
-
-                    //System.out.println(bin);
-
-                    //System.out.printf("Min: %f, Max: %f, Min Avg: %f, Max Avg: %f\n", pMin, pMax, pMinAvg, pMaxAvg);
-                    //System.out.printf("Min: %f, Max: %f, Min Avg: %f, Max Avg: %f\n", fMin, fMax, fMinAvg, fMaxAvg);
-                    float centroidFeatures = 0;
-                    if (centroid.getFeatures() > 0) {
-                        centroidFeatures = centroid.getFeatures();
-                    }
-
-                    double interval = (2 * Math.PI) / (fMinAvg - fMaxAvg) > 0 ? (fMinAvg - fMaxAvg) : (fMaxAvg - fMinAvg);
-                    Color color = Color.rgb(
-                            (int) (127 * Math.sin((interval * centroidFeatures)) + 127),
-                            (int) (127 * Math.sin((interval * centroidFeatures) + 2 * (Math.PI / 3))) + 127,
-                            (int) (127 * Math.sin((interval * centroidFeatures) + 4 * (Math.PI / 3))) + 127
-                    );
-
-                    //System.out.printf("Red: %f, Green: %f, Blue: %f\n", color.getRed(), color.getGreen(), color.getBlue());
-
-                    getLight()
-                            .setBrightness((int)(254 * (pMax / 50)) < 255 ? (int)(254 * (pMax / 50)) : 254)
-                            .setColor(color)
-                            .setTransitionTime(200)
-                            .show();
+        if (!audioContext.isRunning()) {
+            audioContext.start();
+        }
+        float[] powerSpectrumFeatures = powerSpectrum.getFeatures();
+        if (powerSpectrumFeatures != null) {
+            int idx = 1;
+            float pMin = 100;
+            float pMax = 0;
+            float fMin = 20000;
+            float fMax = 20;
+            float cfMin = 20000;
+            float cfMax = 20;
+            for (float power : powerSpectrumFeatures) {
+                //float freq = idx * (19980 / 256);
+                if (power > pMax && power != pMax) {
+                    pMax = power;
+                    //fMax = freq;
+                }
+                if (power < pMin && power != pMin) {
+                    pMin = power;
+                    //fMin = freq;
+                    idx++;
                 }
             }
+
+            if (frequency.getFeatures() != null) {
+                float freq = frequency.getFeatures();
+                if (freq > fMax) {
+                    fMax = freq;
+                }
+                if (freq < fMin) {
+                    fMin = freq;
+                }
+            }
+
+            if (centroid.getFeatures() != null) {
+                float cent = centroid.getFeatures();
+                if (cent > cfMax) {
+                    cfMax = cent;
+                }
+                if (cent < cfMin) {
+                    cfMin = cent;
+                }
+            }
+
+            pMinSum += pMin;
+            pMaxSum += pMax;
+            fMinSum += fMin;
+            fMaxSum += fMax;
+            cfMinSum += cfMin;
+            cfMaxSum += cfMax;
+
+            pMinAvg = pMinSum / numOfSamples;
+            pMaxAvg = pMaxSum / numOfSamples;
+            fMinAvg = fMinSum / numOfSamples;
+            fMaxAvg = fMaxSum / numOfSamples;
+            cfMinAvg = cfMinSum / numOfSamples;
+            cfMaxAvg = cfMaxSum / numOfSamples;
+
+            numOfSamples++;
+
+            double freqInterval = (2 * Math.PI) / (fMaxAvg - fMinAvg);
+            Color frequencyColor = Color.WHITE;
+            if (frequency.getFeatures() != null) {
+                frequencyColor = Color.rgb(
+                        (int) (127 * Math.sin((freqInterval * frequency.getFeatures())) + 127),
+                        (int) (127 * Math.sin((freqInterval * frequency.getFeatures()) + 2 * (Math.PI / 3))) + 127,
+                        (int) (127 * Math.sin((freqInterval * frequency.getFeatures()) + 4 * (Math.PI / 3))) + 127
+                );
+            }
+
+            double centroidInterval = (2 * Math.PI) / (cfMaxAvg - cfMinAvg);
+            Color centroidColor = Color.WHITE;
+            if (centroid.getFeatures() != null) {
+                centroidColor = Color.rgb(
+                        (int) (127 * Math.sin((centroidInterval * centroid.getFeatures())) + 127),
+                        (int) (127 * Math.sin((centroidInterval * centroid.getFeatures()) + 2 * (Math.PI / 3)) + 127),
+                        (int) (127 * Math.sin((centroidInterval * centroid.getFeatures()) + 4 * (Math.PI / 3)) + 127)
+                );
+            }
+
+            Color weighedCentFreqAvg = Color.rgb(
+                    (int) (255 * ((centroidColor.getRed() + (frequencyColor.getRed() * 3)) / 4)),
+                    (int) (255 * ((centroidColor.getGreen() + (frequencyColor.getGreen() * 3)) / 4)),
+                    (int) (255 * ((centroidColor.getBlue() + (frequencyColor.getBlue() * 3)) / 4))
+            );
+
+            /*
+            System.out.printf("Red: %f, Green: %f, Blue: %f\n",
+                    (centroidColor.getRed() + (frequencyColor.getRed() * 2)) / 3,
+                    ((centroidColor.getGreen() + (frequencyColor.getGreen() * 2)) / 3), ((centroidColor.getBlue() + (frequencyColor.getBlue() * 2)) / 3));
+            */
+
+            getLight()
+                    .setBrightness((int) (254 * (pMax / 12000)) < 255 ? (int) (254 * (pMax / 12000)) : 254)
+                    .setColor(weighedCentFreqAvg)
+                    .setTransitionTime(200)
+                    .show();
         }
     }
 
